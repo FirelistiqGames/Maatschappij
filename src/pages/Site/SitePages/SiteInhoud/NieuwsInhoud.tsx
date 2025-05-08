10c3a9f4865c515574735f26e705dd2a07dd14a3
FirelistiqGames <Firelistiq.games@gmail.com>
2025-05-08 21:44:51 +0200

    t

--- src/pages/Site/SitePages/SiteInhoud/NieuwsInhoud.tsx
+++ src/pages/Site/SitePages/SiteInhoud/NieuwsInhoud.tsx
@@ -5,7 +5,7 @@ import { NewsCard } from "@/components/ui/NewsCard";
 
 export function NieuwsOverzicht() {
   return (
-    <div className="mt-15 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4">
+    <div className="mt-15 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4 mb-5">
       {newsItems.map((item, index) => (
         <NewsCard key={index} item={item} />
       ))}
