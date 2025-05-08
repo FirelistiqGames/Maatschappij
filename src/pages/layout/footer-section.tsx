244230ce9af5b365859d982d372fa4d3efbea68e
FirelistiqGames <Firelistiq.games@gmail.com>
2025-05-08 17:24:03 +0200

    tes

--- src/pages/layout/footer-section.tsx
+++ src/pages/layout/footer-section.tsx
@@ -41,7 +41,7 @@ function Footerdemo() {
           <div className="relative">
             <h2 className="mb-4 text-3xl text-red-700 font-bold tracking-tight">Blijf op de hoogte!</h2>
             <p className="mb-6 text-muted-foreground text-red-700">
-              Meld je aan voor onze niewsbrief1!
+              Meld U aan voor onze niewsbrief!
             </p>
             <form className="relative">
               <Input
