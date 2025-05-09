45d632c70f01ec1b537ffa59200a4c0bad281066
FirelistiqGames <Firelistiq.games@gmail.com>
2025-05-09 09:34:18 +0200

    nieuwsopzet

--- src/pages/Site/SitePages/SiteInhoud/Nieuws/NieuwsArtikelen/OpgerichtInhoud.tsx
+++ src/pages/Site/SitePages/SiteInhoud/Nieuws/NieuwsArtikelen/OpgerichtInhoud.tsx
@@ -1,3 +1,4 @@
+import { Separator } from "@radix-ui/react-separator"
 
 
 
@@ -5,22 +6,27 @@ export function OpgerichtInhoud() {
     return (
         <>
             <div className="mt-15">
-                <div className="w-full">
+                <div className="">
 
                     <img
                         src={`${import.meta.env.BASE_URL}banner-light.png`}
                         alt="Banner for light mode"
-                        className="w-full h-auto block dark:hidden"
+                        className="w-full lg:w-4/5 mx-auto h-auto block dark:hidden border border-gray-300 dark:border-gray-600 rounded-sm"
                     />
 
 
                     <img
                         src={`${import.meta.env.BASE_URL}banner-dark.png`}
                         alt="Banner for dark mode"
-                        className="w-full h-auto hidden dark:block"
+                        className="w-full lg:w-4/5 mx-auto h-auto hidden dark:block border border-gray-300 dark:border-gray-600 rounded-sm"
                     />
                 </div>
-                <div className="ml-3">
+                
+                <div className="w-full lg:w-4/5 mx-auto ">
+                    <h1 className="text-xl text-red-700 font-semibold">Title</h1>
+                    <p className="text-muted-foreground">Door: </p></div>
+                <div className="w-full lg:w-5/6 border-t border-gray-300 dark:border-gray-700 my-6 mx-auto" />
+                <div className="w-full lg:w-4/5 mx-auto">
 
                     <p>
                         test
